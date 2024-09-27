c9631eeaba9e4c7289dde7a21a0489799b8dad3c
Alejandro <alejandropenuela825@gmail.com>
2024-09-26 20:51:17 -0500

    Primeros componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './layout/components/footer/footer.component';
+import { NavbarComponent } from './layout/components/navbar/navbar.component';
+import { SearchbarComponent } from './layout/components/searchbar/searchbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FooterComponent,NavbarComponent,SearchbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

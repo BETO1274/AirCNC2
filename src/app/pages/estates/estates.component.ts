178ef1ba6e9ec863dbe5f26da06a25dc55fd901e
Alejandro <alejandropenuela825@gmail.com>
2024-09-27 14:00:16 -0500

    COMPONENTES Y PAGINAS

---
+++ src/app/pages/estates/estates.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estates',
+  standalone: true,
+  imports: [],
+  templateUrl: './estates.component.html',
+  styleUrl: './estates.component.css'
+})
+export class EstatesComponent {
+
+}
